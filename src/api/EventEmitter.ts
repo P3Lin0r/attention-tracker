f7c6a9188e5ec1bba7c4c04e87532488970440d7
P3Lin0r <ermacenkovila@gmail.com>
2026-06-05 22:23:50 +0300

    Feat(api): add off, once and emoveAllListeners methods to EventEmitter

--- src/api/EventEmitter.ts
+++ src/api/EventEmitter.ts
@@ -22,15 +22,53 @@ export class EventEmitter<T extends Record<string, any[]>> {
     }
 
     /**
+     * Unsubscribes a callback function from a specific event.
+     *
+     * @template {keyof T} K 
+     * @param {K} event The name of the event unsubscribe from.
+     * @param {(...args: T[K]) => void} fn The callback to remove from listeners. 
+     * @returns {this} The emitter instance for chaining.
+     */
+    off<K extends keyof T>(event: K, fn: (...args: T[K]) => void): this {
+        const eventListeners = this.listeners[event]
+        if (eventListeners){
+            this.listeners[event] = eventListeners.filter(f => f !== fn)
+        }
+        return this
+    }
+
+    /**
      * Fires an event, triggering all subscribed listeners with the provided arguments.
      *
      * @template {keyof T} K 
-     * @param {K} event The name of the event to trigger.
+     * @param {K} event The name of the event to listen for.
      * @param {...T[K]} args The typed arguments to pass to the listeners.
-     * @returns {boolean} True if the event was successfully dispatched.
+     * @returns {boolean} Always returns true if listeners were called.
      */
-    emit<K extends keyof T>(event: K, ...args: T[K]): boolean {
+    emit<K extends keyof T>(event: K, ...args: T[K]): true {
         this.listeners[event]?.forEach(fn => fn(...args));
         return true;
+    }
+
+    /**
+     * Subscribes a callback that will be executed only once,
+     * then automatically unsubscribed after execution.
+     *
+     * @template {keyof T} K 
+     * @param {K} event The name of the event to listen for.
+     * @param {(...args: T[K]) => void} fn The callback to execute on first event trigger.
+     * @returns {this} The emitter instance for chaining.
+     */
+    once<K extends keyof T>(event: K, fn: (...args: T[K]) => void): this {
+        const wrapper = (...args: T[K]) => {
+            fn(...args)
+            this.off(event, wrapper)
+        }
+        this.on(event, wrapper)
+        return this
+    }
+    
+    removeAllListeners(): void {
+        this.listeners = {}
     }
 }
